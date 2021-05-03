cb0fb6290cc2f4edaa6e48bf5abb2a1cb3fabb58
yyan <yyan@aquent.com>
2021-05-03 11:46:59 -0400

    mixin comment

--- source/collections/collections_ordereddict_move_to_end.py
+++ source/collections/collections_ordereddict_move_to_end.py
@@ -6,8 +6,9 @@
 """Re-ordering an OrderedDict
 """
 
-#end_pymotw_header
+# end_pymotw_header
 import collections
+from inspect import getmro
 
 d = collections.OrderedDict(
     [('a', 'A'), ('b', 'B'), ('c', 'C')]
@@ -28,3 +29,25 @@ d.move_to_end('b', last=False)
 print('\nmove_to_end(last=False):')
 for k, v in d.items():
     print(k, v)
+
+
+class Mixin1:
+    def test(self):
+        print("Mixin1")
+
+
+class Mixin2:
+    def test(self):
+        print("Mixin2")
+
+
+class BaseClass:
+    pass
+
+
+class MyClass(BaseClass, Mixin1, Mixin2):
+    pass
+
+
+print(MyClass.__mro__)
+print(getmro(MyClass))
